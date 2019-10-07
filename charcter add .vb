11f5faf767bd4c7ee66f87d5804abfa0ac71a9fc
Zain1972 <55274977+Zain1972@users.noreply.github.com>
2019-10-08 01:33:41 +0500

    Add files via upload

---
+++ charcter add .vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, Str2, Char1, Char2, NextChar As String
+        Dim Counter As Integer
+
+        Str1 = ""
+        Str2 = ""
+        Char1 = ""
+        Char2 = ""
+        NextChar = ""
+        Counter = 0
+
+        Console.WriteLine(" enter string ")
+        Str1 = Console.ReadLine
+
+        Console.WriteLine(" enter character to remove ")
+        Char1 = Console.ReadLine
+
+        Console.WriteLine(" enter charater to add ")
+        Char2 = Console.ReadLine
+
+        For Counter = 1 To Len(Str1)
+            NextChar = Mid(Str1, Counter, 1)
+            If NextChar <> Char1 Then
+                Str2 = Str2 & NextChar
+            Else
+                If NextChar = Char1 Then
+                    Str2 = Str2 & Char2
+                End If
+            End If
+        Next
+        Console.WriteLine(Str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
